d15c7d02473b4e98f242201f56d6aadf9be3981f
DufanD <quraishdufan@rocketmail.com>
2019-04-23 13:54:39 +0700

    add visualization

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import csv
+import matplotlib.pyplot as plt
 
-with open('data/ruspini.csv', 'r') as csvFile:
-    reader = csv.reader(csvFile)
-    for row in reader:
-        print(row)
+import numpy as np
+import pandas as pd
 
-csvFile.close()
+df = pd.read_csv('data/ruspini.csv')
+
+# Visualization Ruspini Dataset
+plt.scatter(df['x'].values, df['y'].values, color='r')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.show()
+
+# with open('data/ruspini.csv', 'r') as csvFile:
+#     reader = csv.reader(csvFile)
+#     for row in reader:
+#         print(row)
+
+# csvFile.close()
