e7ccc5d03765115f71d100a7c20a35f5deeda35b
dinopuguh <puguhmunung16@gmail.com>
2019-04-23 13:47:19 +0700

    mengisi missing

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import csv
+import pandas as pd
+import numpy as np
+import impyute as impy
+
 
 with open('data/ruspini.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)
-    for row in reader:
-        print(row)
+    # for row in reader:
+    # print(row)
 
 csvFile.close()
+
+
+# DPPS
+ruspini_missing = list()
+new_ruspini = list()
+
+with open('data/data_ruspini_missing.csv', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    for row in reader:
+        ruspini_missing.append(row)
+csvFile.close()
+
+for i, itemi in enumerate(ruspini_missing):
+    for j, itemj in enumerate(ruspini_missing[i]):
+        if(itemj == '?'):
+            ruspini_missing[i][j] = np.nan
+        else:
+            ruspini_missing[i][j] = float(itemj)
+
+print(ruspini_missing)
+print(impy.mean(np.array(ruspini_missing)))
