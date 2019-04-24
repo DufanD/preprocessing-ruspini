b94f9ff360b1f908e8dad46693698ca02b7de05e
dinopuguh <puguhmunung16@gmail.com>
2019-04-24 08:23:05 +0700

    fill missing values

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import impyute as impy
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,49 +6,21 @@ import pandas as pd
 
 df = pd.read_csv('data/ruspini.csv')
 
-# Visualization Ruspini Dataset
 plt.scatter(df['x'].values, df['y'].values, color='r')
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
 
-# with open('data/ruspini.csv', 'r') as csvFile:
-#     reader = csv.reader(csvFile)
-#     for row in reader:
-#         print(row)
-
-# csvFile.close()
-
-
-with open('data/ruspini.csv', 'r') as csvFile:
-    reader = csv.reader(csvFile)
-    # for row in reader:
-    # print(row)
-
-csvFile.close()
-
-
-# DPPS
-ruspini_missing = list()
-new_ruspini = list()
-
 ruspini_missing = pd.read_csv('data/data_ruspini_missing.csv')
+ruspini_missing = ruspini_missing.replace('?', np.nan)
 
-# with open('data/data_ruspini_missing.csv', 'r') as csvFile:
-#     reader = csv.reader(csvFile)
-#     for row in reader:
-#         ruspini_missing.append(row)
-# csvFile.close()
-
+new_ruspini = impy.fast_knn(np.array(ruspini_missing, dtype=float))
 print(ruspini_missing)
+print(new_ruspini)
 
-# for i, itemi in enumerate(ruspini_missing):
-#     for j, itemj in enumerate(ruspini_missing[i]):
-#         if(itemj == '?'):
-#             ruspini_missing[i][j] = np.nan
-#         else:
-#             ruspini_missing[i][j] = float(itemj)
+# for data in new_ruspini:
+#     # x, y = data
+#     print(data)
 
-# new_ruspini = impy.fast_knn(np.array(ruspini_missing))
-# print(ruspini_missing)
-# print(new_ruspini)
+# plt.scatter(x, y, color='g')
+# plt.show()
