0d780a703cff5f006eb01447e57526feebd8e50b
Dino Puguh <puguhmunung16@gmail.com>
2019-04-28 21:22:39 +0700

    update missing visualization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+#%%
 import impyute as impy
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,27 +8,36 @@ df = pd.read_csv('data/ruspini.csv')
 
 ruspini_missing = pd.read_csv('data/data_ruspini_missing.csv')
 ruspini_missing = ruspini_missing.replace('?', np.nan)
+ruspini_missing = np.array(ruspini_missing, dtype=float)
 
-new_ruspini = impy.fast_knn(np.array(ruspini_missing, dtype=float))
+new_ruspini = impy.mean(ruspini_missing)
+ruspini_missing = pd.DataFrame({
+    'x': ruspini_missing[:, 0],
+    'y': ruspini_missing[:, 1],
+    'label': ruspini_missing[:, 2]
+})
+
 print(ruspini_missing)
 print(new_ruspini)
 
-new_ruspini = pd.DataFrame(
-    {'x': new_ruspini[:, 0], 'y': new_ruspini[:, 1], 'label': new_ruspini[:, 2]})
+new_ruspini = pd.DataFrame({
+    'x': new_ruspini[:, 0],
+    'y': new_ruspini[:, 1],
+    'label': new_ruspini[:, 2]
+})
 
 plt.figure('Ruspini Missing')
-plt.subplot(111)
-plt.scatter(df['x'].values, df['y'].values,
-            c=df['label'].values)
+plt.scatter(ruspini_missing['x'].values,
+            ruspini_missing['y'].values,
+            c=ruspini_missing['label'].values)
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.legend(loc='upper left')
 
 plt.figure('New Ruspini')
 plt.scatter(new_ruspini['x'].values,
-            new_ruspini['y'].values, c=new_ruspini['label'].values)
+            new_ruspini['y'].values,
+            c=new_ruspini['label'].values)
 
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.legend(loc='upper left')
 plt.show()
