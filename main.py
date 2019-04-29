fc3a803e964c1671f5a52fbbc3ac90251a8a7b93
DufanD <quraishdufan@rocketmail.com>
2019-04-29 12:06:08 +0700

    add visualzation ruspini origin

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('data/ruspini.csv')
+init_ruspini = pd.read_csv('data/ruspini.csv')
+init_ruspini = np.array(init_ruspini, dtype=float)
+init_ruspini = pd.DataFrame({
+    'x': init_ruspini[:, 0],
+    'y': init_ruspini[:, 1],
+    'label': init_ruspini[:, 2]
+})
 
 ruspini_missing = pd.read_csv('data/data_ruspini_missing.csv')
 ruspini_missing = ruspini_missing.replace('?', np.nan)
@@ -17,16 +23,22 @@ ruspini_missing = pd.DataFrame({
     'label': ruspini_missing[:, 2]
 })
 
-print(ruspini_missing)
-print(new_ruspini)
-
 new_ruspini = pd.DataFrame({
     'x': new_ruspini[:, 0],
     'y': new_ruspini[:, 1],
     'label': new_ruspini[:, 2]
 })
 
-colors = ['red', 'green', 'blue', 'white']
+print(init_ruspini)
+print(ruspini_missing)
+print(new_ruspini)
+
+plt.figure('Ruspini Origin')
+plt.scatter(init_ruspini['x'].values,
+            init_ruspini['y'].values,
+            c=init_ruspini['label'].values)
+plt.xlabel('X')
+plt.ylabel('Y')
 
 plt.figure('Ruspini Missing')
 plt.scatter(ruspini_missing['x'].values,
