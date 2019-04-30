93a9f9d9f52ef664c65f1130449245309d9192ca
Dino Puguh <puguhmunung16@gmail.com>
2019-04-30 13:51:04 +0700

    update imputation with grouping

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import impyute as impy
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pdb
+from sklearn.neighbors import KNeighborsClassifier
 
 init_ruspini = pd.read_csv('data/ruspini.csv')
 init_ruspini = np.array(init_ruspini, dtype=float)
@@ -16,17 +18,26 @@ ruspini_missing = pd.read_csv('data/data_ruspini_missing.csv')
 ruspini_missing = ruspini_missing.replace('?', np.nan)
 ruspini_missing = np.array(ruspini_missing, dtype=float)
 
-new_ruspini = impy.fast_knn(ruspini_missing, k=3)
 ruspini_missing = pd.DataFrame({
     'x': ruspini_missing[:, 0],
     'y': ruspini_missing[:, 1],
     'label': ruspini_missing[:, 2]
 })
 
+ruspini_missing_grouped = ruspini_missing.groupby('label')
+
+new_ruspini_grouped = list()
+for key, item in ruspini_missing_grouped:
+    temp = list(impy.fast_knn(np.array(item), k=3))
+    for i in temp:
+        new_ruspini_grouped.append(i)
+
+new_ruspini_grouped = np.array(new_ruspini_grouped)
+#%%
 new_ruspini = pd.DataFrame({
-    'x': new_ruspini[:, 0],
-    'y': new_ruspini[:, 1],
-    'label': new_ruspini[:, 2]
+    'x': new_ruspini_grouped[:, 0],
+    'y': new_ruspini_grouped[:, 1],
+    'label': new_ruspini_grouped[:, 2]
 })
 
 print(init_ruspini)
